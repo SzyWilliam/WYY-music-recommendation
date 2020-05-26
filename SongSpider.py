9150fd08b913965adfb0aad7ac5f9bf37f5b94a3
Song ZiYang/William Song <szywilliam@foxmail.com>
2020-05-26 11:24:54 +0800

    small bugs fixed

--- SongSpider.py
+++ SongSpider.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import  expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+import threading
 def debug_print_thread(msg, exe=True):
     if exe: print('[*', threading.get_ident(), '*]', msg)
 
