73c818dd451611f46a48760b7352c7dcf50c4b03
Song ZiYang/William Song <szywilliam@foxmail.com>
2020-05-26 12:20:33 +0800

    small bugs fixed

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -2,13 +2,14 @@ import requests
 from lxml import etree
 import time
 def get_all_proxy():
-    url = 'http://www.xicidaili.com/nn/1'
+    url = 'https://www.xicidaili.com'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
     }
     response = requests.get(url, headers=headers)
     html_ele = etree.HTML(response.text)
     ip_eles = html_ele.xpath('//table[@id="ip_list"]/tr/td[2]/text()')
+    # print(ip_eles)
     port_ele = html_ele.xpath('//table[@id="ip_list"]/tr/td[3]/text()')
     proxy_list = []
     for i in range(0,len(ip_eles)):
@@ -18,6 +19,7 @@ def get_all_proxy():
 
 def check_all_proxy(proxy_list):
     valid_proxy_list = []
+    # proxy_list=["http://120.132.52.141:8888"]
     for proxy in proxy_list:
         url = 'http://music.163.com/'
         proxy_dict = {
@@ -26,7 +28,9 @@ def check_all_proxy(proxy_list):
         try:
             start_time = time.time()
             response = requests.get(url, proxies=proxy_dict, timeout=5)
-            if response.status_code == 200:
+            contents = response.content.decode('utf-8')
+            if response.status_code == 200 and contents.find("n-for404") == -1:
+                # print(contents)
                 end_time = time.time()
                 print('代理可用：' + proxy)
                 print('耗时:' + str(end_time - start_time))
@@ -41,6 +45,7 @@ def check_all_proxy(proxy_list):
 
 if __name__ == '__main__':
     proxy_list = get_all_proxy()
+    # proxy_list = []
     valid_proxy_list = check_all_proxy(proxy_list)
     print('--'*30)
     print(valid_proxy_list)
